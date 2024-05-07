5ac32f1cf8f86e0f6f04058789364b8df9d40551
Iaroslav Siniugin <iaroslav.upwork@gmail.com>
2024-05-07 10:49:17 +0300

    feat: add tests to check the debouncer for  a search field

---
+++ test/features/gallery/presentation/widgets/search_field_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gallery_app/features/gallery/presentation/widgets/search_field.dart';
+
+String _callbackValue = '';
+const _query = 'query';
+
+void main() {
+  setUp(() => _callbackValue = '');
+
+  group(
+    'GIVEN $SearchField to test callbacks',
+    () {
+      testWidgets(
+        'WHEN entered a text '
+        'AND not settled for 500 milliseconds '
+        'THEN a callback value should be an empty string',
+        (tester) async {
+          await tester.appPumpWidget(
+            onChanged: (text) => _callbackValue = text,
+          );
+
+          await tester.enterText(find.byType(TextField), _query);
+
+          expect(_callbackValue, '');
+        },
+      );
+
+      testWidgets(
+        'WHEN entered a text '
+        'AND settled for 500 milliseconds '
+        'THEN a callback value should match the entered text',
+        (tester) async {
+          await tester.appPumpWidget(
+            onChanged: (text) => _callbackValue = text,
+          );
+
+          await tester.enterText(find.byType(TextField), _query);
+          await tester.pumpAndSettle(const Duration(milliseconds: 500));
+
+          expect(_callbackValue, _query);
+        },
+      );
+    },
+  );
+}
+
+extension on WidgetTester {
+  Future<void> appPumpWidget({
+    required ValueChanged<String> onChanged,
+  }) async {
+    return pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: SearchField(
+            onSearch: onChanged,
+          ),
+        ),
+      ),
+    );
+  }
+}
